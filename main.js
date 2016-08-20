b6e0304b987db6d842a44a0761f6376f8fb7fa07
Nathan Jensby <nathanjensby@Nathans-MacBook-Air.local>
2016-08-19 20:56:14 -0500

    Moved main and style to wdi-fundamentals

---
+++ main.js
@@ -0,0 +1,12 @@
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "queen";
+
+if (cardOne === cardFour) {
+	alert("You found a match!");
+}
+	else {
+		alert("Sorry, try again.")
+	}
+console.log("JS file is connected to HTML! Woo!")
