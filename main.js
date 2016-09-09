97c269ae5977260bb76c26bb2fd0121d02591fa4
Nathan Jensby <nathanjensby@Nathans-MacBook-Air.local>
2016-09-09 14:45:54 -0500

    added board and createBoard function

--- main.js
+++ main.js
@@ -3,10 +3,18 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "queen";
 
-if (cardOne === cardFour) {
+/*var cards = ['queen', 'queen','king','king'];*/
+/*if (cardOne === cardFour) {
 	alert("You found a match!");
 }
 	else {
 		alert("Sorry, try again.")
 	}
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!")*/
+
+var board = document.getElementById("game-board");
+for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('div');
+	cardElement.className = 'card';
+	cardElement.appendChild(.board);
+}
